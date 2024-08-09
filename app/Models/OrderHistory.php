5a25b9d475d68bdf729ef7272e5cd0d05cae3394
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2024-08-09 10:42:03 +0530

    Create OrderHistory.php

---
+++ app/Models/OrderHistory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderHistory extends Model
+{
+    use HasFactory;
+
+    protected $table = 'order_histories';
+
+    protected $fillable = [
+        'order_id',
+        'order_status',
+        'payment_method',
+
+    ];
+
+    
+}
